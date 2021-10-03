ca219f4c6d16e4b632b391853b7d99fc2ab14689
Alexey Ermakov <strangebrew@yandex.ru>
2021-10-03 13:54:24 +0000

    Translated using Weblate (Russian)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: CutefishOS/Screenshot
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/screenshot/ru/

--- translations/ru_RU.ts
+++ translations/ru_RU.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../src/screenshotview.cpp" line="96"/>
         <source>The picture has been saved to %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Изображение сохранено в %1</translation>
     </message>
     <message>
         <location filename="../src/screenshotview.cpp" line="127"/>
         <source>The picture has been saved to the clipboard</source>
-        <translation type="unfinished"></translation>
+        <translation>Изображение сохранено в буфер обмена</translation>
     </message>
 </context>
 </TS>
