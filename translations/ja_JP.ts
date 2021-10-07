61a8549630742f44a970725305faee2bd67ffe8e
TaIenT <1054001563@qq.com>
2021-10-07 18:47:47 +0000

    Translated using Weblate (Japanese)
    Currently translated at 0.0% (0 of 2 strings)
    
    Translation: CutefishOS/Screenshot
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/screenshot/ja/

--- translations/ja_JP.ts
+++ translations/ja_JP.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../src/screenshotview.cpp" line="96"/>
         <source>The picture has been saved to %1</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">絵は %1 に保存されました</translation>
     </message>
     <message>
         <location filename="../src/screenshotview.cpp" line="127"/>
         <source>The picture has been saved to the clipboard</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">画像がクリップボードに保存されました</translation>
     </message>
 </context>
 </TS>
