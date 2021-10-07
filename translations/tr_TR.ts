c19e66f54d625be342bf9281d35681150207335d
Halit Sever <dunyayuvarlakmuratbabandir@gmail.com>
2021-10-07 17:16:13 +0000

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: CutefishOS/Screenshot
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/screenshot/tr/

--- translations/tr_TR.ts
+++ translations/tr_TR.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../src/screenshotview.cpp" line="96"/>
         <source>The picture has been saved to %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Resim %1 klasörüne kaydedildi</translation>
     </message>
     <message>
         <location filename="../src/screenshotview.cpp" line="127"/>
         <source>The picture has been saved to the clipboard</source>
-        <translation type="unfinished"></translation>
+        <translation>Resim panoya kaydedildi</translation>
     </message>
 </context>
 </TS>
