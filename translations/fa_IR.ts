ec4e3fa14c1fec0816555913c60bda44fe2b8367
Ali Frh <saintalifrh@disroot.org>
2021-10-20 22:20:34 +0000

    Translated using Weblate (Persian)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: CutefishOS/Screenshot
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/screenshot/fa/

--- translations/fa_IR.ts
+++ translations/fa_IR.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../src/screenshotview.cpp" line="96"/>
         <source>The picture has been saved to %1</source>
-        <translation type="unfinished"></translation>
+        <translation>تصویر در %1 ذخیره شد</translation>
     </message>
     <message>
         <location filename="../src/screenshotview.cpp" line="127"/>
         <source>The picture has been saved to the clipboard</source>
-        <translation type="unfinished"></translation>
+        <translation>تصویر در کلیپ بورد کپی شد</translation>
     </message>
 </context>
 </TS>
