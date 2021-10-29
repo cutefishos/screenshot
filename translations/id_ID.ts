6d0c2b88d3852bb629b0a12db509fdb75a7be141
vcyzteen <vcyzscape@gmail.com>
2021-10-29 20:41:44 +0700

    Translated using Weblate (Indonesia)

--- translations/id_ID.ts
+++ translations/id_ID.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../src/screenshotview.cpp" line="96"/>
         <source>The picture has been saved to %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Gambar telah disimpan di %1</translation>
     </message>
     <message>
         <location filename="../src/screenshotview.cpp" line="127"/>
         <source>The picture has been saved to the clipboard</source>
-        <translation type="unfinished"></translation>
+        <translation>Gambar telah disalin ke clipboard</translation>
     </message>
 </context>
 </TS>
