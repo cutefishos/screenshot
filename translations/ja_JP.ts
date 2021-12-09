808834388f56befad1eb612c058e5fdfddf7a46f
Jun Nogata <nogajun@gmail.com>
2021-12-09 14:54:47 +0000

    Translated using Weblate (Japanese)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: CutefishOS/Screenshot
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/screenshot/ja/

--- translations/ja_JP.ts
+++ translations/ja_JP.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../src/screenshotview.cpp" line="96"/>
         <source>The picture has been saved to %1</source>
-        <translation>絵は %1 に保存されました</translation>
+        <translation>画像は %1に保存されました</translation>
     </message>
     <message>
         <location filename="../src/screenshotview.cpp" line="127"/>
         <source>The picture has been saved to the clipboard</source>
-        <translation>画像がクリップボードに保存されました</translation>
+        <translation>画像はクリップボードに保存されました</translation>
     </message>
 </context>
 </TS>
