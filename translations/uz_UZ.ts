60441a1aa215a809302d6afe8994f7fa0e0e041e
Shohqadam Ergashev <ergashevshohqadam@gmail.com>
2022-01-23 06:48:10 +0000

    Translated using Weblate (Uzbek)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: CutefishOS/Screenshot
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/screenshot/uz/

--- translations/uz_UZ.ts
+++ translations/uz_UZ.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../src/screenshotview.cpp" line="96"/>
         <source>The picture has been saved to %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Rasm %1 ga saqlandi</translation>
     </message>
     <message>
         <location filename="../src/screenshotview.cpp" line="127"/>
         <source>The picture has been saved to the clipboard</source>
-        <translation type="unfinished"></translation>
+        <translation>Rasm Buferga saqlandi</translation>
     </message>
 </context>
 </TS>
