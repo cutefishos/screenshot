d7681b70f8f0e7f8c15044b52769eff5bb52d0e7
Arty Wavee Ganacci <zulrig33@gmail.com>
2022-04-22 23:49:10 +0000

    Translated using Weblate (Spanish (Mexico))
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: CutefishOS/Screenshot
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/screenshot/es_MX/

--- translations/es_MX.ts
+++ translations/es_MX.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../src/screenshotview.cpp" line="96"/>
         <source>The picture has been saved to %1</source>
-        <translation type="unfinished"></translation>
+        <translation>La imagen se ha guardado en %1</translation>
     </message>
     <message>
         <location filename="../src/screenshotview.cpp" line="127"/>
         <source>The picture has been saved to the clipboard</source>
-        <translation type="unfinished"></translation>
+        <translation>La imagen se ha guardado en el portapapeles</translation>
     </message>
 </context>
 </TS>
