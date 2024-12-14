ca8b17921e99206e8e70e994ab3c726201c2cbdd
தமிழ்நேரம் <anishprabu.t@gmail.com>
2024-12-14 16:16:24 +0000

    Translated using Weblate (Tamil)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: CutefishOS/Screenshot
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/screenshot/ta/

--- translations/ta_IN.ts
+++ translations/ta_IN.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../src/screenshotview.cpp" line="96"/>
         <source>The picture has been saved to %1</source>
-        <translation type="unfinished"></translation>
+        <translation>படம் %1 என சேமிக்கப்பட்டுள்ளது</translation>
     </message>
     <message>
         <location filename="../src/screenshotview.cpp" line="127"/>
         <source>The picture has been saved to the clipboard</source>
-        <translation type="unfinished"></translation>
+        <translation>படம் கிளிப்போர்டில் சேமிக்கப்பட்டுள்ளது</translation>
     </message>
 </context>
 </TS>
