d81ac23d75d28b78f7cbdbc5b013372507da8f79
Ricky Tigg <ricky.tigg@gmail.com>
2025-02-28 11:37:23 +0100

    Translated using Weblate (Finnish)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: CutefishOS/Screenshot
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/screenshot/fi/

--- translations/fi_FI.ts
+++ translations/fi_FI.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../src/screenshotview.cpp" line="96"/>
         <source>The picture has been saved to %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Kuva on tallennettu kohteeseen %1</translation>
     </message>
     <message>
         <location filename="../src/screenshotview.cpp" line="127"/>
         <source>The picture has been saved to the clipboard</source>
-        <translation type="unfinished"></translation>
+        <translation>Kuva on tallennettu leikepöydälle</translation>
     </message>
 </context>
 </TS>
